4dab5bf571656ba6a023e51cf9fe39ccb4c52cf8
sakib <s.sarder232002@gmail.com>
2023-06-07 04:28:49 +0600

    firebase initial set up done

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBeff0oNlznYFhfPLcD2Bv4VpsRNSDXl4U",
+  authDomain: "craftopia-4e8ba.firebaseapp.com",
+  projectId: "craftopia-4e8ba",
+  storageBucket: "craftopia-4e8ba.appspot.com",
+  messagingSenderId: "803934292568",
+  appId: "1:803934292568:web:eb6862dec8407cbb25182d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;

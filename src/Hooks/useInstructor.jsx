2268749b4d8821f7d411f7cce3525757d83e0365
sakib <s.sarder232002@gmail.com>
2023-06-12 16:55:36 +0600

    firebase deploy and useInstructor page modified

--- src/Hooks/useInstructor.jsx
+++ src/Hooks/useInstructor.jsx
@@ -9,6 +9,9 @@ const useInstructor = () => {
     const { data: isAdmin, isLoading:isInstructorLoading} = useQuery({
         queryKey: ["isAdmin", user?.email],
         queryFn: async () => {
+            if (!user) {
+                return false;
+            }
             const res = await axiosSecure.get(`/users/instructor/${user?.email}`)
             console.log("is ins", res.data);
             return res.data.instructor;
